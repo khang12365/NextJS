81c65c37848b676693ee6893bffe51f1828d5546
khang <khangnguyen9819@gmail.com>
2025-08-20 14:38:51 +0700

    router

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
+export default Home;
---
+++ src/app/admin/pagee/page.tsx
@@ -0,0 +1,8 @@
+const Hello = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
+export default Hello;
